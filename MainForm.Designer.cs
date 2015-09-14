47c2d64a4e0015d5c8f842627cea485a0f2173d3
TSOKeefe <tokeefe@harding.edu>
2015-09-14 14:04:39 -0500

    added Visual studio project

---
+++ MainForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace LightsOut
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewGameButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(255, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "&Game";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.x3ToolStripMenuItem,
+            this.x4ToolStripMenuItem,
+            this.x5ToolStripMenuItem});
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "&New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // x3ToolStripMenuItem
+            // 
+            this.x3ToolStripMenuItem.CheckOnClick = true;
+            this.x3ToolStripMenuItem.Name = "x3ToolStripMenuItem";
+            this.x3ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.x3ToolStripMenuItem.Text = "&3x3";
+            this.x3ToolStripMenuItem.Click += new System.EventHandler(this.x3ToolStripMenuItem_Click);
+            // 
+            // x4ToolStripMenuItem
+            // 
+            this.x4ToolStripMenuItem.CheckOnClick = true;
+            this.x4ToolStripMenuItem.Name = "x4ToolStripMenuItem";
+            this.x4ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.x4ToolStripMenuItem.Text = "&4x4";
+            this.x4ToolStripMenuItem.Click += new System.EventHandler(this.x4ToolStripMenuItem_Click);
+            // 
+            // x5ToolStripMenuItem
+            // 
+            this.x5ToolStripMenuItem.CheckOnClick = true;
+            this.x5ToolStripMenuItem.Name = "x5ToolStripMenuItem";
+            this.x5ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.x5ToolStripMenuItem.Text = "&5x5";
+            this.x5ToolStripMenuItem.Click += new System.EventHandler(this.x5ToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // NewGameButton
+            // 
+            this.NewGameButton.Location = new System.Drawing.Point(26, 235);
+            this.NewGameButton.Name = "NewGameButton";
+            this.NewGameButton.Size = new System.Drawing.Size(75, 23);
+            this.NewGameButton.TabIndex = 1;
+            this.NewGameButton.Text = "New Game";
+            this.NewGameButton.UseVisualStyleBackColor = true;
+            this.NewGameButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NewGameButton_MouseClick);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(157, 235);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ExitButton_MouseClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 261);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.NewGameButton);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Lights Out!";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button NewGameButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.ToolStripMenuItem x3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x5ToolStripMenuItem;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightsOut
+{
+    public partial class MainForm : Form
+    {
+        private const int DEFAULT = 3;
+        private int NUM_CELLS = DEFAULT;
+        private const int GRID_OFFSET = 25;
+        private const int GRID_LENGTH = 200;
+        private const int NUM_CELLS_3X3 = 3;
+        private const int NUM_CELLS_4X4 = 4;
+        private const int NUM_CELLS_5X5 = 5;
+        private int CELL_LENGTH = GRID_LENGTH / NUM_CELLS_3X3;
+
+        private bool [,] grid;
+        private Random rand;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+         
+ 
+            rand = new Random();
+            grid = new bool[NUM_CELLS, NUM_CELLS];
+
+            // Turn entire grid on
+            for (int r = 0; r < NUM_CELLS; r++)
+              for (int c = 0; c < NUM_CELLS; c++)
+                 grid[r, c] = true;
+  
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+
+            if (x3ToolStripMenuItem.Checked)
+            {
+                NUM_CELLS = DEFAULT;
+                CELL_LENGTH = GRID_LENGTH / NUM_CELLS;
+                grid = new bool[NUM_CELLS, NUM_CELLS];
+
+            }
+
+            if (x4ToolStripMenuItem.Checked)
+            {
+               
+                NUM_CELLS = NUM_CELLS_4X4;
+                CELL_LENGTH = GRID_LENGTH / NUM_CELLS;
+                grid = new bool[NUM_CELLS, NUM_CELLS];
+            }
+
+            if (x5ToolStripMenuItem.Checked)
+            {
+                NUM_CELLS = NUM_CELLS_5X5;
+                CELL_LENGTH = GRID_LENGTH / NUM_CELLS;
+                grid = new bool[NUM_CELLS, NUM_CELLS];
+            }
+            
+
+            for (int r = 0; r < NUM_CELLS; r++)
+                for (int c = 0; c < NUM_CELLS; c++)
+                {
+                    Brush brush;
+                    Pen pen;
+
+                    if (grid[r,c])
+                    {
+                        pen = Pens.Black;
+                        brush = Brushes.White;
+                    }
+                    else
+                    {
+                        pen = Pens.White;
+                        brush = Brushes.Black;
+                    }
+                  
+
+                    int x = c * CELL_LENGTH + GRID_OFFSET;
+                    int y = r * CELL_LENGTH + GRID_OFFSET;
+
+                    g.DrawRectangle(pen, x, y, CELL_LENGTH, CELL_LENGTH);
+                    g.FillRectangle(brush, x + 1, y + 1, CELL_LENGTH - 1, CELL_LENGTH - 1);
+
+                    
+                }
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Make sure click was inside the grid
+            if (e.X < GRID_OFFSET || e.X > CELL_LENGTH * NUM_CELLS + GRID_OFFSET ||
+            e.Y < GRID_OFFSET || e.Y > CELL_LENGTH * NUM_CELLS + GRID_OFFSET)
+                return; 
+
+            // Find row, col of mouse press
+            int r = (e.Y - GRID_OFFSET) / CELL_LENGTH;
+            int c = (e.X - GRID_OFFSET) / CELL_LENGTH; 
+
+            for (int i = r-1; i < r+ 1; i++)
+                for (int j = c-1; j < c+1; j++)
+                {
+                    if (i>= 0 && i < NUM_CELLS && j >= 0 && j <= NUM_CELLS)
+                    {
+                        grid[i, j] = !grid[i, j];
+                    }
+                }
+
+            this.Invalidate();
+
+            if (PlayerWon())
+            {
+                // Display winner dialog box
+                MessageBox.Show(this, "Congratulations! You've won!", "Lights Out!",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+        }
+
+        private bool PlayerWon()
+        {
+            bool WinCondition = true;
+
+            for (int r = 0; r < NUM_CELLS; r++)
+                for (int c = 0; c < NUM_CELLS; c++)
+                {
+                    if (grid[r,c])
+                    {
+                        WinCondition = false;
+                    }
+                }
+            return WinCondition;
+        }
+
+        
+
+      
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGameButton_MouseClick(sender, e);
+        }
+
+        private void NewGameButton_MouseClick(object sender, EventArgs e)
+        {
+            if (x3ToolStripMenuItem.Checked)
+            {
+                NUM_CELLS = DEFAULT;
+                CELL_LENGTH = GRID_LENGTH / NUM_CELLS;
+                grid = new bool[NUM_CELLS, NUM_CELLS];
+                rand = new Random();
+
+            }
+
+            if (x4ToolStripMenuItem.Checked)
+            {
+                NUM_CELLS = NUM_CELLS_4X4;
+                CELL_LENGTH = GRID_LENGTH / NUM_CELLS;
+                grid = new bool[NUM_CELLS, NUM_CELLS];
+                rand = new Random();
+            }
+
+            if (x5ToolStripMenuItem.Checked)
+            {
+                NUM_CELLS = NUM_CELLS_5X5;
+                CELL_LENGTH = GRID_LENGTH / NUM_CELLS;
+                grid = new bool[NUM_CELLS, NUM_CELLS];
+                rand = new Random();
+            }
+
+            // Turn entire grid on
+            for (int r = 0; r < NUM_CELLS; r++)
+                for (int c = 0; c < NUM_CELLS; c++)
+                    grid[r, c] = true;
+
+            
+            // Fill grid with either white or black
+            for (int r = 0; r < NUM_CELLS; r++)
+                for (int c = 0; c < NUM_CELLS; c++)
+                    grid[r, c] = rand.Next(2) == 1;
+            // Redraw grid
+            this.Invalidate(); 
+            
+        }
+
+        private void ExitButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutForm aboutBox = new AboutForm();
+            aboutBox.ShowDialog(this);
+        }
+
+        private void x3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            x3ToolStripMenuItem.Checked = true;
+            x4ToolStripMenuItem.Checked = false;
+            x5ToolStripMenuItem.Checked = false;
+        }
+
+        private void x4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            x4ToolStripMenuItem.Checked = true;
+            x3ToolStripMenuItem.Checked = false;
+            x5ToolStripMenuItem.Checked = false;
+        }
+
+        private void x5ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            x5ToolStripMenuItem.Checked = true;
+            x4ToolStripMenuItem.Checked = false;
+            x3ToolStripMenuItem.Checked = false;
+        }
+
+      
+
+
+    }
+}
